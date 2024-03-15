9c98d122ac136995f31f9cccfb31bd76fb0d4572
colbycade <colbycade@gmail.com>
2024-03-15 00:35:34 -0600

    mongodb initial config

--- database.js
+++ database.js
@@ -1,110 +1,136 @@
-// Database implementation (in-memory for now)
+// Database implementation using MongoDB
 
-// Example database data
-let users = [
-  { username: 'john_doe', familyCode: 'a273B1', role: 'Parent', profilePic: null },
-  { username: 'jane_doe', familyCode: 'a273B1', role: 'Child', profilePic: null },
-  { username: 'jill_doe', familyCode: 'a273B1', role: 'Child', profilePic: null }
-];
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
 
-let families = {
-  "a273B1": {
-    "Family": [{ name: "Take Sally to School", dueDate: "2024-02-29", completed: true },
-    { name: "Clean the kitchen", dueDate: "", completed: false },
-    { name: "Take out the trash", dueDate: "2024-03-02", completed: false }],
-    "john_doe": [{ name: "Buy groceries", dueDate: "2024-03-01", completed: false },
-    { name: "Doctor's appointment", dueDate: "2024-03-05", completed: false }],
-    "jane_doe": [],
-    "jill_doe": [{ name: "Feed the dog", dueDate: "2024-03-01", completed: false }]
-  }
-};
+const uri = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+
+let client, db, user_collection, family_collection;
+
+async function main() {
+  // Connect to the database cluster
+  const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+  client = new MongoClient(url);
+  await client.connect();
+
+  db = client.db('familytracker');
+  user_collection = db.collection('user');
+  family_collection = db.collection('family');
+
+  // Test the connection
+  await db.command({ ping: 1 });
+}
+main().catch(console.error);
+
+
+
+
+
 
 // In-memory implementation
 
+// Example database data
+// let users = [
+//   { username: 'john_doe', familyCode: 'a273B1', role: 'Parent', profilePic: null },
+//   { username: 'jane_doe', familyCode: 'a273B1', role: 'Child', profilePic: null },
+//   { username: 'jill_doe', familyCode: 'a273B1', role: 'Child', profilePic: null }
+// ];
 
-// User and family data functions
+// let families = {
+//   "a273B1": {
+//     "Family": [{ name: "Take Sally to School", dueDate: "2024-02-29", completed: true },
+//     { name: "Clean the kitchen", dueDate: "", completed: false },
+//     { name: "Take out the trash", dueDate: "2024-03-02", completed: false }],
+//     "john_doe": [{ name: "Buy groceries", dueDate: "2024-03-01", completed: false },
+//     { name: "Doctor's appointment", dueDate: "2024-03-05", completed: false }],
+//     "jane_doe": [],
+//     "jill_doe": [{ name: "Feed the dog", dueDate: "2024-03-01", completed: false }]
+//   }
+// };
 
-function getUser(username) {
-  return users.find(user => user.username === username);
-}
+// // User and family data functions
 
-function getFamily(familyCode) {
-  return users.filter(user => user.familyCode === familyCode);
-}
+// function getUser(username) {
+//   return users.find(user => user.username === username);
+// }
 
-function addFamilyMember(newMember) {
-  users.push(newMember);
-}
+// function getFamily(familyCode) {
+//   return users.filter(user => user.familyCode === familyCode);
+// }
 
-function removeFamilyMember(familyCode, username) {
-  users = users.filter(user => user.username !== username || user.familyCode !== familyCode);
-}
+// function addFamilyMember(newMember) {
+//   users.push(newMember);
+// }
 
-function changeFamilyMemberRole(familyCode, username) {
-  const user = users.find(user => user.username === username && user.familyCode === familyCode);
-  if (user) {
-    user.role = user.role === 'Parent' ? 'Child' : 'Parent';
-    return user;
-  }
-  return null;
-}
+// function removeFamilyMember(familyCode, username) {
+//   users = users.filter(user => user.username !== username || user.familyCode !== familyCode);
+// }
 
-function getUserFamilyCode(username) {
-  const user = users.find(user => user.username === username);
-  return user ? user.familyCode : null;
-}
+// function changeFamilyMemberRole(familyCode, username) {
+//   const user = users.find(user => user.username === username && user.familyCode === familyCode);
+//   if (user) {
+//     user.role = user.role === 'Parent' ? 'Child' : 'Parent';
+//     return user;
+//   }
+//   return null;
+// }
 
-// Task list functions
+// function getUserFamilyCode(username) {
+//   const user = users.find(user => user.username === username);
+//   return user ? user.familyCode : null;
+// }
 
-function getFamilyTaskLists(familyCode) {
-  return families[familyCode] || {};
-}
+// // Task list functions
 
-function getFamilyTaskList(familyCode, listName) {
-  const familyTaskLists = getFamilyTaskLists(familyCode);
-  return familyTaskLists[listName] || null;
-}
+// function getFamilyTaskLists(familyCode) {
+//   return families[familyCode] || {};
+// }
 
-function updateTaskList(familyCode, listName, updatedTasks) {
-  if (families[familyCode]) {
-    families[familyCode][listName] = updatedTasks;
-  }
-}
+// function getFamilyTaskList(familyCode, listName) {
+//   const familyTaskLists = getFamilyTaskLists(familyCode);
+//   return familyTaskLists[listName] || null;
+// }
 
-function deleteTaskList(familyCode, listName) {
-  if (families[familyCode]) {
-    delete families[familyCode][listName];
-  }
-}
+// function updateTaskList(familyCode, listName, updatedTasks) {
+//   if (families[familyCode]) {
+//     families[familyCode][listName] = updatedTasks;
+//   }
+// }
 
-function createTask(familyCode, listName, newTask) {
-  const taskList = getFamilyTaskList(familyCode, listName);
-  if (taskList) {
-    taskList.push(newTask);
-  }
-}
+// function deleteTaskList(familyCode, listName) {
+//   if (families[familyCode]) {
+//     delete families[familyCode][listName];
+//   }
+// }
 
-function updateProfilePicture(username, profilePicData) {
-  const user = getUser(username);
-  if (user) {
-    user.profilePic = profilePicData;
-  }
-}
+// function createTask(familyCode, listName, newTask) {
+//   const taskList = getFamilyTaskList(familyCode, listName);
+//   if (taskList) {
+//     taskList.push(newTask);
+//   }
+// }
+
+// function updateProfilePicture(username, profilePicData) {
+//   const user = getUser(username);
+//   if (user) {
+//     user.profilePic = profilePicData;
+//   }
+// }
 
 
-// Export for use in API using CommonJS module system
+// // Export for use in API using CommonJS module system
 
-module.exports = {
-  getUser,
-  getFamilyTaskLists,
-  getFamilyTaskList,
-  updateTaskList,
-  createTask,
-  updateProfilePicture,
-  getFamily,
-  addFamilyMember,
-  removeFamilyMember,
-  changeFamilyMemberRole,
-  getUserFamilyCode,
-  deleteTaskList
-};
+// module.exports = {
+//   getUser,
+//   getFamilyTaskLists,
+//   getFamilyTaskList,
+//   updateTaskList,
+//   createTask,
+//   updateProfilePicture,
+//   getFamily,
+//   addFamilyMember,
+//   removeFamilyMember,
+//   changeFamilyMemberRole,
+//   getUserFamilyCode,
+//   deleteTaskList
+// };
