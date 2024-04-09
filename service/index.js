e989cc632e634e25dde9ae793bfd9156d80d1cc7
colbycade <colbycade@gmail.com>
2024-04-08 22:44:30 -0600

    configure vite

--- service/index.js
+++ service/index.js
@@ -27,7 +27,7 @@ const { dbConnect, getUser, getUserByToken, getFamilyTaskLists, getFamilyTaskLis
   loginUser, registerNewFamily, registerJoinFamily } = require('./database');
 
 // The service port. In production the front-end code is statically hosted by the service on the same port.
-const port = process.argv.length > 2 ? process.argv[2] : 8080;
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
 // JSON body parsing using built-in middleware
 app.use(express.json());
