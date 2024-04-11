09c0caba25d81b395e7f50e89db45690c10b40e1
colbycade <colbycade@gmail.com>
2024-04-10 19:01:41 -0600

    add router for menu

--- src/app.jsx
+++ src/app.jsx
@@ -1,34 +1,48 @@
 import React from 'react';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Home } from './home/home';
+import { About } from './about/about';
+
 
 export default function App() {
     return (
-        <div className='body'>
-            <header>
-                <div className="logotitle">
-                    <img src="assets/favicon.ico" alt="logo" width="100" height="100" />
-                    <h1>Family Tracker</h1>
-                </div>
-                <nav>
-                    <div className="menu">
-                        <li><a href="index.html">Login</a></li>
-                        <li><a className="current-page" href="home.html">Home</a></li>
-                        <li><a href="about.html">About</a></li>
+        <BrowserRouter>
+            <div className='body'>
+                <header>
+                    <div className="logotitle">
+                        <img src="assets/favicon.ico" alt="logo" width="100" height="100" />
+                        <h1>Family Tracker</h1>
                     </div>
-                    {/* <div className="log-out">
+                    <nav>
+                        <div className="menu">
+                            <li><NavLink className='nav-link' to=''>Login</NavLink></li>
+                            <li><NavLink className='nav-link' to='home'>Home</NavLink></li>
+                            <li><NavLink className='nav-link' to='about'>About</NavLink></li>
+                        </div>
+                        {/* <div className="log-out">
                         <button id="log-out-button" onClick={logout}>Logout</button>
                     </div> */}
-                </nav>
-            </header>
-            <main>
-                App components go here
-            </main>
-            <footer>
-                <hr />
-                <span> Made by Colby Wright</span>
-                <br />
-                <a href="https://github.com/colbycade/startup">GitHub</a>
-            </footer>
-        </div>
+                    </nav>
+                </header>
+                <Routes>
+                    <Route path='/' element={<Login />} exact />
+                    <Route path='/home' element={<Home />} />
+                    <Route path='/about' element={<About />} />
+                    <Route path='*' element={<NotFound />} />
+                </Routes>
+                <footer>
+                    <hr />
+                    <span> Made by Colby Wright</span>
+                    <br />
+                    <a href="https://github.com/colbycade/startup">GitHub</a>
+                </footer>
+            </div>
+        </BrowserRouter >
     );
+}
+
+function NotFound() {
+    return <main>404: Return to sender. Address unknown.</main>;
 }
