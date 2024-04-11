f453dde31274cb21cd3dd6ceb3023f16301382cb
colbycade <colbycade@gmail.com>
2024-04-10 21:36:23 -0600

    html of event log component

---
+++ src/home/eventLog.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const EventLog = () => {
+    return (
+        <div id="event-log">
+            <h2>Event Log</h2>
+            <div id="events">
+                {/* Dynamically rendered event log entries */}
+            </div>
+        </div>
+    );
+};
+
+export default EventLog;
